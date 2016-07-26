5d7d5508c6cf16e7c3b2e103dd47dbbefe1bf0c8
Brandon Unbedacht <unbedachtb@gmail.com>
2016-07-26 15:13:16 -0600

    finish

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = 9000;
+var messages = [];
+app.get('/', function(req, res) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+app.listen(port, function() {
+    console.log("Listening on port ", port);
+});
+app.use(bodyParser.json());
+app.post('/', function(req, res) {
+    console.log(req.body);
+    messages.push({
+      message: req.body.message,
+      time: new Date()
+    });
 
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+app.options('/', function(req, res) {
+    res.status(200).set({
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }).send(JSON.stringify(messages));
+});
